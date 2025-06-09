078e16022f29e4480438d7a99809506ab36efde5
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-06-09 04:35:11 -0700

    commit mesage: quick fix

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"], // targets all 5 HTML files directly
   theme: {
     extend: {},
   },
@@ -8,3 +8,22 @@ module.exports = {
 }
 
 
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./0x02-tailwind-css/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
